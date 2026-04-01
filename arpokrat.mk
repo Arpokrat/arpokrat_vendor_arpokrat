a9124c29e031c0fed542ad24a772301df7ebbdbd
Arpokrat <info@arpokrat.com>
2026-04-01 06:24:38 +0000

    Cleaning overlays

--- arpokrat.mk
+++ arpokrat.mk
@@ -1,5 +1,41 @@
+PRODUCT_NAME := arpokrat_device
+PRODUCT_BRAND := Arpokrat
+PRODUCT_MANUFACTURER := Arpokrat
+PRODUCT_MODEL := Arpokrat Phone
+
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.sys.usb.defaultconfig=none \
+    persist.sys.usb.config=none
+
+
 PRODUCT_PACKAGES += \
-    ArpokratFrameworkOverlay
+    Orbot \
+    ArpokratFrameworkOverlay \
+    ArpokratAuditorOverlay \
+    ArpokratCalculatorOverlay \
+    ArpokratCameraOverlay \
+    ArpokratClockOverlay \
+    ArpokratContactsOverlay \
+    ArpokratFilesOverlay \
+    ArpokratGalleryOverlay \
+    ArpokratMessagingOverlay \
+    ArpokratPdfViewerOverlay \
+    ArpokratSettingsOverlay \
+    ArpokratSystemUIOverlay \
+    ArpokratDialerOverlay \
+    ArpokratSetupWizardOverlay \
+    ArpokratSetupOverlay \
+    ArpokratProviderOverlay \
+    ArpokratSettingsThemeOverlay \
+    ArpokratKeyboardOverlay \
+    ArpokratLauncherOverlay \
+    ArpokratLatinIMEOverlay
 
 PRODUCT_COPY_FILES += \
-    vendor/arpokrat/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/arpokrat/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/arpokrat/hardware_overrides/empty_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    vendor/arpokrat/hardware_overrides/empty_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+    vendor/arpokrat/hardware_overrides/empty_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    vendor/arpokrat/hardware_overrides/empty_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    vendor/arpokrat/hardware_overrides/empty_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
